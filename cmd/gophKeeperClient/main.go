5ebb20f15932b078e769c77230e9d8259712c9e4
a.kraynov <a.kraynov@astrio.net>
2025-01-16 12:28:06 +0400

    добавлены get, delete, edit операции

--- cmd/gophKeeperClient/main.go
+++ cmd/gophKeeperClient/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"errors"
@@ -9,19 +10,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
 const CommandSet = "Список команд:\n" +
 	"Регистрация нового пользователя: register\n" +
 	"Аутентификация пользователя: login\n" +
-	"Синхронизация данных с сервером: synchronize\n" +
-	"Добавить новые данные: addData\n" +
+	"Добавить новые данные: add\n" +
+	"Получеие даннных пользователя: get\n" +
+	"Удалить данные по id: delete\n" +
+	"Изменить данные по id: edit\n" +
 	"Информация о версии и дате сборки бинарного файла клиента: info"
 
 const cookieName string = "user_info"
 
 var token string
+
+// UserData представляет структуру таблицы user_data.
+type UserData struct {
+	ID       uint
+	UserID   uint
+	UserData string
+	DataType string
+}
 
 func main() {
 	var name string
@@ -34,14 +46,21 @@ func main() {
 		}
 
 		switch name {
-		case "addData":
+		case "add":
 			addData()
+		case "get":
+			_, err = getData()
+			if err != nil {
+				return
+			}
+		case "edit":
+			editData()
+		case "delete":
+			deleteData()
 		case "register":
 			register()
 		case "login":
 			authenticate()
-		case "synchronize":
-			fmt.Println(CommandSet)
 		case "info":
 			fmt.Println(CommandSet)
 		case "ping":
@@ -52,32 +71,212 @@ func main() {
 	}
 }
 
-func addData() {
-	var answer string
+func deleteData() {
+	var dataID int
+	fmt.Println("Введите id записи на удаление")
+
 	for {
-		fmt.Println("Хотите ввести метаинформацию к данным? yes/no")
-		_, err := fmt.Scan(&answer)
-
+		_, err := fmt.Scan(&dataID)
 		if err != nil {
-			return
-		}
-
-		if answer == "yes" || answer == "no" {
-			break
+			fmt.Println("Ошибка чтения id. Повторите попытку")
 		} else {
-			fmt.Println("Введите 'да' или 'нет'")
+			break
 		}
 	}
 
-	var metaInfo string
+	req, err := http.NewRequest("GET", "http://localhost:8080/delete_data?id="+strconv.Itoa(dataID), nil)
 
-	if answer == "yes" {
-		fmt.Println("Введите метаинформацию")
-		_, err := fmt.Scan(&metaInfo)
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
 
-		if err != nil {
-			return
+	req.Header.Set("Content-Type", "application/json")
+	cookie := &http.Cookie{
+		Name:  cookieName,
+		Value: token,
+	}
+
+	req.AddCookie(cookie)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	defer func() {
+		if resp != nil {
+			_ = resp.Body.Close()
 		}
+	}()
+
+	if err != nil {
+		log.Printf("Ошибка при выполнении запроса: %v", err)
+	}
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		log.Printf("Необходимо авторизоваться для работы с хранилищем!")
+		return
+	}
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Ошибка при чтении ответа: %v", err)
+	}
+
+	fmt.Printf("Ответ сервера: %s\n", responseBody)
+}
+
+func editData() {
+	userDataArray, err := getData()
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	var dataID uint
+	fmt.Println("Введите id записи на изменение")
+
+	for {
+		_, err := fmt.Scan(&dataID)
+		if err != nil {
+			fmt.Println("Ошибка чтения id. Повторите попытку")
+		} else {
+			break
+		}
+	}
+
+	metaInfo, err := enterMetaInfo()
+
+	if err != nil {
+		log.Fatalf("Ошибка ввода")
+	}
+
+	var jsonData []byte
+	var data map[string]interface{}
+	var dataType string
+	for _, dataRow := range userDataArray {
+		if dataRow.ID != dataID {
+			continue
+		}
+
+		if dataRow.DataType == "log-pas" {
+			data, err = enterKeyPas(metaInfo)
+		} else if dataRow.DataType == "text" {
+			data, err = enterText(metaInfo)
+		} else if dataRow.DataType == "binary" {
+			data, err = enterBinary(metaInfo)
+		}
+
+		dataType = dataRow.DataType
+		break
+	}
+
+	if err != nil {
+		log.Fatalf("Ошибка ввода")
+	}
+
+	jsonData, err = json.Marshal(data)
+	if err != nil {
+		log.Fatalf("Error marshaling JSON: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", "http://localhost:8080/edit_data", bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	req.Header.Set("Data-Id", strconv.FormatUint(uint64(dataID), 10))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Data-Type", dataType)
+
+	cookie := &http.Cookie{
+		Name:  cookieName,
+		Value: token,
+	}
+
+	req.AddCookie(cookie)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	defer func() {
+		if resp != nil {
+			_ = resp.Body.Close()
+		}
+	}()
+
+	if err != nil {
+		log.Printf("Ошибка при выполнении запроса: %v", err)
+		return
+	}
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		log.Printf("Необходимо авторизоваться для работы с хранилищем!")
+		return
+	}
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Ошибка при чтении ответа: %v", err)
+		return
+	}
+
+	fmt.Printf("Ответ сервера: %s\n", responseBody)
+}
+
+func getData() ([]UserData, error) {
+	req, err := http.NewRequest("GET", "http://localhost:8080/get_data", nil)
+
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	cookie := &http.Cookie{
+		Name:  cookieName,
+		Value: token,
+	}
+
+	req.AddCookie(cookie)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	defer func() {
+		if resp != nil {
+			_ = resp.Body.Close()
+		}
+	}()
+
+	if err != nil {
+		log.Printf("Ошибка при выполнении запроса: %v", err)
+	}
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		log.Printf("Необходимо авторизоваться для работы с хранилищем!")
+		return nil, err
+	}
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Ошибка при чтении ответа: %v", err)
+		return nil, err
+	}
+
+	var userDataArray []UserData
+	err = json.Unmarshal(responseBody, &userDataArray)
+
+	if err != nil {
+		log.Printf("Ошибка при чтении ответа: %v", err)
+		return nil, err
+	}
+
+	for _, userData := range userDataArray {
+		fmt.Printf("ID: %d, UserID: %d, Data: %s, DataType: %s\n", userData.ID, userData.UserID, userData.UserData, userData.DataType)
+	}
+
+	return userDataArray, nil
+}
+
+func addData() {
+	metaInfo, err := enterMetaInfo()
+	if err != nil {
+		return
 	}
 
 	var infoType string
@@ -87,7 +286,7 @@ func addData() {
 			"Текстовые данные: text\n" +
 			"Бинарные данные: binary\n" +
 			"Выход: exit")
-		_, err := fmt.Scan(&infoType)
+		_, err = fmt.Scan(&infoType)
 
 		if err != nil {
 			return
@@ -113,50 +312,53 @@ func addData() {
 			}
 			break
 		case "exit":
-			return
+			break
 		default:
 			fmt.Println("Вы ввели несуществующую команду.\n" + CommandSet)
 		}
 	}
 }
 
+func enterMetaInfo() (string, error) {
+	var answer string
+	for {
+		fmt.Println("Хотите ввести метаинформацию к данным? yes/no")
+		_, err := fmt.Scanln(&answer)
+
+		if err != nil {
+			fmt.Println("Ошибка ввода")
+			return "", err
+		}
+
+		if answer == "yes" || answer == "no" {
+			break
+		} else {
+			fmt.Println("Введите 'да' или 'нет'")
+		}
+	}
+
+	var metaInfo string
+
+	if answer == "yes" {
+		fmt.Println("Введите метаинформацию")
+		reader := bufio.NewReader(os.Stdin)
+
+		var err error
+		metaInfo, err = reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Ошибка ввода")
+			return "", err
+		}
+	}
+
+	return metaInfo, nil
+}
+
 func addKeyPas(metaInfo string) error {
-	var login string
-	var password string
-	for {
-		fmt.Println("Введите логин:")
-		_, err := fmt.Scan(&login)
+	data, err := enterKeyPas(metaInfo)
 
-		if err != nil {
-			return err
-		}
-
-		if login == "" {
-			fmt.Println("Введите непустую строку!")
-		} else {
-			break
-		}
-	}
-
-	for {
-		fmt.Println("Введите пароль:")
-		_, err := fmt.Scan(&password)
-
-		if err != nil {
-			return err
-		}
-
-		if login == "" {
-			fmt.Println("Введите непустую строку!")
-		} else {
-			break
-		}
-	}
-
-	data := map[string]string{
-		"login":     login,
-		"password":  password,
-		"meta-info": metaInfo,
+	if err != nil {
+		log.Fatalf("Ошибка ввода")
 	}
 
 	jsonData, err := json.Marshal(data)
@@ -193,13 +395,13 @@ func addKeyPas(metaInfo string) error {
 	}
 
 	if resp.StatusCode == http.StatusUnauthorized {
-		log.Printf("Необходимо авторизоваться для работы с хранилищем!")
-		return errors.New("Необходимо авторизоваться для работы с хранилищем!")
+		log.Printf("Необходимо авторизоваться для работы с хранилищем")
+		return errors.New("Необходимо авторизоваться для работы с хранилищем")
 	}
 
 	responseBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf("Ошибка при чтении ответа: %v", err)
+		log.Printf("ошибка при чтении ответа: %v", err)
 	}
 
 	fmt.Printf("Ответ сервера: %s\n", responseBody)
@@ -207,26 +409,53 @@ func addKeyPas(metaInfo string) error {
 	return nil
 }
 
-func addText(metaInfo string) error {
-	var text string
+func enterKeyPas(metaInfo string) (map[string]interface{}, error) {
+	var login string
+	var password string
 	for {
-		fmt.Println("Введите текст:")
-		_, err := fmt.Scan(&text)
+		fmt.Println("Введите логин:")
+		_, err := fmt.Scan(&login)
 
 		if err != nil {
-			return err
+			return nil, err
 		}
 
-		if text == "" {
+		if login == "" {
 			fmt.Println("Введите непустую строку!")
 		} else {
 			break
 		}
 	}
 
-	data := map[string]string{
-		"text":      text,
+	for {
+		fmt.Println("Введите пароль:")
+		_, err := fmt.Scan(&password)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if login == "" {
+			fmt.Println("Введите непустую строку!")
+		} else {
+			break
+		}
+	}
+
+	data := map[string]interface{}{
+		"login":     login,
+		"password":  password,
 		"meta-info": metaInfo,
+	}
+
+	return data, nil
+}
+
+func addText(metaInfo string) error {
+	data, err := enterText(metaInfo)
+
+	if err != nil {
+		return err
 	}
 
 	jsonData, err := json.Marshal(data)
@@ -277,18 +506,36 @@ func addText(metaInfo string) error {
 	return nil
 }
 
-func addBinary(metaInfo string) error {
-	var data []byte
+func enterText(metaInfo string) (map[string]interface{}, error) {
+	var text string
+	for {
+		fmt.Println("Введите текст:")
+		_, err := fmt.Scan(&text)
 
-	fmt.Println("Введите бинарные данные (Ctrl+D для завершения):")
-	data, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		return fmt.Errorf("ошибка чтения из стандартного ввода: %v", err)
+		if err != nil {
+			return nil, err
+		}
+
+		if text == "" {
+			fmt.Println("Введите непустую строку!")
+		} else {
+			break
+		}
 	}
 
-	dataInfo := map[string]interface{}{
-		"meta-info":   metaInfo,
-		"binary-data": data,
+	data := map[string]interface{}{
+		"text":      text,
+		"meta-info": metaInfo,
+	}
+
+	return data, nil
+}
+
+func addBinary(metaInfo string) error {
+	dataInfo, err := enterBinary(metaInfo)
+
+	if err != nil {
+		log.Fatalf("Ошибка ввода")
 	}
 
 	jsonData, err := json.Marshal(dataInfo)
@@ -338,6 +585,23 @@ func addBinary(metaInfo string) error {
 	fmt.Printf("Ответ сервера: %s\n", responseBody)
 
 	return nil
+}
+
+func enterBinary(metaInfo string) (map[string]interface{}, error) {
+	var data []byte
+
+	fmt.Println("Введите бинарные данные (Ctrl+D для завершения):")
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка чтения из стандартного ввода: %v", err)
+	}
+
+	dataInfo := map[string]interface{}{
+		"meta-info":   metaInfo,
+		"binary-data": data,
+	}
+
+	return dataInfo, nil
 }
 
 func ping() {
